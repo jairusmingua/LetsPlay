acd24444f69b67c9c7ae4d364f054a89c7a3a6e9
Jairus <minguajairus@yahoo.com.ph>
2019-10-23 00:21:36 +0800

    final

--- LetsPlay/Form1.Designer.cs
+++ LetsPlay/Form1.Designer.cs
@@ -28,22 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.door1btn = new System.Windows.Forms.Button();
             this.door2btn = new System.Windows.Forms.Button();
             this.door3btn = new System.Windows.Forms.Button();
             this.msgLbl = new System.Windows.Forms.Label();
-            this.resetBtn = new System.Windows.Forms.Button();
             this.door4btn = new System.Windows.Forms.Button();
             this.door5btn = new System.Windows.Forms.Button();
+            this.yesBtn = new System.Windows.Forms.Button();
+            this.noBtn = new System.Windows.Forms.Button();
+            this.label0 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // door1btn
             // 
+            this.door1btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("door1btn.BackgroundImage")));
             this.door1btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.door1btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.door1btn.FlatAppearance.BorderSize = 0;
             this.door1btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.door1btn.Location = new System.Drawing.Point(81, 126);
+            this.door1btn.Location = new System.Drawing.Point(83, 181);
             this.door1btn.Name = "door1btn";
             this.door1btn.Size = new System.Drawing.Size(92, 184);
             this.door1btn.TabIndex = 0;
@@ -52,11 +61,12 @@
             // 
             // door2btn
             // 
+            this.door2btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("door2btn.BackgroundImage")));
             this.door2btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.door2btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.door2btn.FlatAppearance.BorderSize = 0;
             this.door2btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.door2btn.Location = new System.Drawing.Point(183, 126);
+            this.door2btn.Location = new System.Drawing.Point(185, 181);
             this.door2btn.Name = "door2btn";
             this.door2btn.Size = new System.Drawing.Size(92, 184);
             this.door2btn.TabIndex = 1;
@@ -65,11 +75,12 @@
             // 
             // door3btn
             // 
+            this.door3btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("door3btn.BackgroundImage")));
             this.door3btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.door3btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.door3btn.FlatAppearance.BorderSize = 0;
             this.door3btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.door3btn.Location = new System.Drawing.Point(285, 126);
+            this.door3btn.Location = new System.Drawing.Point(287, 181);
             this.door3btn.Name = "door3btn";
             this.door3btn.Size = new System.Drawing.Size(92, 184);
             this.door3btn.TabIndex = 2;
@@ -78,33 +89,25 @@
             // 
             // msgLbl
             // 
-            this.msgLbl.AutoSize = true;
+            this.msgLbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.msgLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.msgLbl.Location = new System.Drawing.Point(36, 32);
+            this.msgLbl.Location = new System.Drawing.Point(0, 32);
             this.msgLbl.Name = "msgLbl";
-            this.msgLbl.Size = new System.Drawing.Size(576, 50);
+            this.msgLbl.Size = new System.Drawing.Size(664, 50);
             this.msgLbl.TabIndex = 3;
             this.msgLbl.Text = "Let\'s play the door challenge. \r\nChoose one of the doors and win your school grad" +
     "e!!!";
             this.msgLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // resetBtn
-            // 
-            this.resetBtn.Location = new System.Drawing.Point(280, 366);
-            this.resetBtn.Name = "resetBtn";
-            this.resetBtn.Size = new System.Drawing.Size(100, 41);
-            this.resetBtn.TabIndex = 4;
-            this.resetBtn.Text = "Reset Game\r\n";
-            this.resetBtn.UseVisualStyleBackColor = true;
-            this.resetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
-            // 
             // door4btn
             // 
+            this.door4btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("door4btn.BackgroundImage")));
             this.door4btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.door4btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.door4btn.FlatAppearance.BorderSize = 0;
             this.door4btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.door4btn.Location = new System.Drawing.Point(387, 126);
+            this.door4btn.Location = new System.Drawing.Point(389, 181);
             this.door4btn.Name = "door4btn";
             this.door4btn.Size = new System.Drawing.Size(92, 184);
             this.door4btn.TabIndex = 5;
@@ -113,29 +116,123 @@
             // 
             // door5btn
             // 
+            this.door5btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("door5btn.BackgroundImage")));
             this.door5btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.door5btn.Cursor = System.Windows.Forms.Cursors.Hand;
             this.door5btn.FlatAppearance.BorderSize = 0;
             this.door5btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.door5btn.Location = new System.Drawing.Point(489, 126);
+            this.door5btn.Location = new System.Drawing.Point(491, 181);
             this.door5btn.Name = "door5btn";
             this.door5btn.Size = new System.Drawing.Size(92, 184);
             this.door5btn.TabIndex = 6;
             this.door5btn.UseVisualStyleBackColor = true;
             this.door5btn.Click += new System.EventHandler(this.Door5btn_Click);
             // 
+            // yesBtn
+            // 
+            this.yesBtn.Location = new System.Drawing.Point(213, 114);
+            this.yesBtn.Name = "yesBtn";
+            this.yesBtn.Size = new System.Drawing.Size(117, 32);
+            this.yesBtn.TabIndex = 7;
+            this.yesBtn.Text = "Yes";
+            this.yesBtn.UseVisualStyleBackColor = true;
+            this.yesBtn.Click += new System.EventHandler(this.YesBtn_Click);
+            // 
+            // noBtn
+            // 
+            this.noBtn.Location = new System.Drawing.Point(336, 114);
+            this.noBtn.Name = "noBtn";
+            this.noBtn.Size = new System.Drawing.Size(117, 32);
+            this.noBtn.TabIndex = 8;
+            this.noBtn.Text = "No";
+            this.noBtn.UseVisualStyleBackColor = true;
+            this.noBtn.Click += new System.EventHandler(this.NoBtn_Click);
+            // 
+            // label0
+            // 
+            this.label0.AutoSize = true;
+            this.label0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label0.Location = new System.Drawing.Point(94, 255);
+            this.label0.Name = "label0";
+            this.label0.Size = new System.Drawing.Size(60, 29);
+            this.label0.TabIndex = 9;
+            this.label0.Text = "Uno";
+            this.label0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label0.Click += new System.EventHandler(this.Label0_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 29);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(298, 255);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 29);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(422, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 29);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(516, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 29);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "1";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(560, 412);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(90, 26);
+            this.resetBtn.TabIndex = 4;
+            this.resetBtn.Text = "Reset Game\r\n";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(662, 450);
-            this.Controls.Add(this.door5btn);
-            this.Controls.Add(this.door4btn);
+            this.Controls.Add(this.noBtn);
+            this.Controls.Add(this.yesBtn);
             this.Controls.Add(this.resetBtn);
             this.Controls.Add(this.msgLbl);
+            this.Controls.Add(this.door4btn);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.door3btn);
-            this.Controls.Add(this.door2btn);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.door1btn);
+            this.Controls.Add(this.label0);
+            this.Controls.Add(this.door5btn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.door2btn);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Door Challenge";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -150,9 +247,16 @@
         private System.Windows.Forms.Button door2btn;
         private System.Windows.Forms.Button door3btn;
         private System.Windows.Forms.Label msgLbl;
-        private System.Windows.Forms.Button resetBtn;
         private System.Windows.Forms.Button door4btn;
         private System.Windows.Forms.Button door5btn;
+        private System.Windows.Forms.Button yesBtn;
+        private System.Windows.Forms.Button noBtn;
+        private System.Windows.Forms.Label label0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button resetBtn;
     }
 }
 
--- LetsPlay/Form1.cs
+++ LetsPlay/Form1.cs
@@ -14,10 +14,12 @@ namespace LetsPlay
 {
     public partial class Form1 : Form
     {
-        bool choice = true;
+        int choice;
         string[] prizes = { "Singko", "Dos", "Tres", "Shift", "Uno" };
         int[] rank;
         string [] doors;
+        Button[] d;
+        Label[] l;
         const int MAX_SIZE = 5;
         const int MAX_BARGAIN = 2;
         int winningDoor = 0;
@@ -26,21 +28,43 @@ namespace LetsPlay
         {
             InitializeComponent();
             doors = new string[MAX_SIZE];
+            d = new Button[MAX_SIZE];
             rank = new int[MAX_SIZE];
+            l = new Label[MAX_SIZE];
             rank[0] = 0;
             rank[1] = 1;
             rank[2] = 2;
             rank[3] = 3;
             rank[4] = 4;
+            l[0] = label0;
+            l[1] = label1;
+            l[2] = label2;
+            l[3] = label3;
+            l[4] = label4;
+            d[0] = door1btn;
+            d[1] = door2btn;
+            d[2] = door3btn;
+            d[3] = door4btn;
+            d[4] = door5btn;
+            choice = 0;
             randomize();
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+            hidePrompt();
             tries = MAX_BARGAIN;
             randomize();
         }
-
+        private void hidePrompt()
+        {
+            yesBtn.Visible = false;
+            noBtn.Visible = false;
+        }
+        private void showPrompt()
+        {
+            yesBtn.Visible = true;
+            noBtn.Visible = true;
+        }
         private void Door1btn_Click(object sender, EventArgs e)
         {
             bargain(0);
@@ -70,14 +94,46 @@ namespace LetsPlay
 
             resetGame();
         }
+        private void changeBtnState(bool state)
+        {
+           for(int i = 0; i<doors.Length; i++)
+            {
+                
+               
+                
+                    d[i].Enabled = state;
+                
+            }
+        }
         private void bargain(int index)
         {
-            msgLbl.Text = doors[index].ToString();
+            string m ="";
+            choice = index;
+            changeBtnState(false);
             tries--;
             if (tries == 1)
             {
+                int r;
+                for(; ; )
+                {
+                    r = new Random().Next(0, MAX_SIZE - 1);
+                    if(r != index && doors[r]!="Uno")
+                    {
+                        break;
+                    }
+                }
+                m = String.Format("Door {0} has {1}.\nDo you want to change your choice???", r+1, doors[r]);
 
+                showPrompt();
+                   
+            }else if (tries == 0)
+            {
+                l[index].Text = doors[index];
+                d[index].Visible = false;
+                showPrompt();
+                m = String.Format("Congratulations you won a {0}!!!\nReset Game?", doors[index]);
             }
+            msgLbl.Text = m;
         }
         private void randomize()
         {
@@ -88,13 +144,52 @@ namespace LetsPlay
             for (int i = 0; i < rank.Length; i++)
             {
                 doors[rank[i]] = prizes[i];
+                
+                
             }
+            winningDoor = rank[MAX_SIZE - 1];
         }
         private void resetGame()
         {
             tries = MAX_BARGAIN;
             randomize();
+            changeBtnState(true);
+            showButtons();
+            hidePrompt();
+            msgLbl.Text = "Let's play the door challenge.\nChoose one of the doors and win your school grade!!!";
+        }
+        private void showButtons()
+        {
+            for(int i = 0; i < doors.Length; i++)
+            {
+                d[i].Visible = true;
+            }
+        }
+        private void YesBtn_Click(object sender, EventArgs e)
+        {
+            string m = "";
+            if (tries == 1)
+            {
+                m = String.Format("Choose another door...");
+                msgLbl.Text = m;
+                changeBtnState(true);
+                hidePrompt();
+            }
+            else if (tries == 0)
+            {
+                resetGame();
+            }
+            
         }
 
+        private void NoBtn_Click(object sender, EventArgs e)
+        {
+            bargain(choice);
+        }
+
+        private void Label0_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

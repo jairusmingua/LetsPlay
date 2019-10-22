069fd8db05599b7bef85486921ee979eeede9bf6
Jairus <minguajairus@yahoo.com.ph>
2019-10-22 21:35:00 +0800

    first commit

---
+++ LetsPlay/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace LetsPlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.door1btn = new System.Windows.Forms.Button();
+            this.door2btn = new System.Windows.Forms.Button();
+            this.door3btn = new System.Windows.Forms.Button();
+            this.msgLbl = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.door4btn = new System.Windows.Forms.Button();
+            this.door5btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // door1btn
+            // 
+            this.door1btn.BackgroundImage = global::LetsPlay.Properties.Resources.water_65425408_Cartoon_wooden_door_vector_illustration_free_download_m;
+            this.door1btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.door1btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.door1btn.FlatAppearance.BorderSize = 0;
+            this.door1btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.door1btn.Location = new System.Drawing.Point(76, 123);
+            this.door1btn.Name = "door1btn";
+            this.door1btn.Size = new System.Drawing.Size(92, 184);
+            this.door1btn.TabIndex = 0;
+            this.door1btn.UseVisualStyleBackColor = true;
+            this.door1btn.Click += new System.EventHandler(this.Door1btn_Click);
+            // 
+            // door2btn
+            // 
+            this.door2btn.BackgroundImage = global::LetsPlay.Properties.Resources.water_65425408_Cartoon_wooden_door_vector_illustration_free_download_m;
+            this.door2btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.door2btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.door2btn.FlatAppearance.BorderSize = 0;
+            this.door2btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.door2btn.Location = new System.Drawing.Point(178, 123);
+            this.door2btn.Name = "door2btn";
+            this.door2btn.Size = new System.Drawing.Size(92, 184);
+            this.door2btn.TabIndex = 1;
+            this.door2btn.UseVisualStyleBackColor = true;
+            this.door2btn.Click += new System.EventHandler(this.Door2btn_Click);
+            // 
+            // door3btn
+            // 
+            this.door3btn.BackgroundImage = global::LetsPlay.Properties.Resources.water_65425408_Cartoon_wooden_door_vector_illustration_free_download_m;
+            this.door3btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.door3btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.door3btn.FlatAppearance.BorderSize = 0;
+            this.door3btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.door3btn.Location = new System.Drawing.Point(280, 123);
+            this.door3btn.Name = "door3btn";
+            this.door3btn.Size = new System.Drawing.Size(92, 184);
+            this.door3btn.TabIndex = 2;
+            this.door3btn.UseVisualStyleBackColor = true;
+            this.door3btn.Click += new System.EventHandler(this.Door3btn_Click);
+            // 
+            // msgLbl
+            // 
+            this.msgLbl.AutoSize = true;
+            this.msgLbl.Location = new System.Drawing.Point(292, 42);
+            this.msgLbl.Name = "msgLbl";
+            this.msgLbl.Size = new System.Drawing.Size(50, 13);
+            this.msgLbl.TabIndex = 3;
+            this.msgLbl.Text = "Message\r\n";
+            this.msgLbl.Click += new System.EventHandler(this.MsgLbl_Click);
+            // 
+            // resetBtn
+            // 
+            this.resetBtn.Location = new System.Drawing.Point(548, 396);
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.Size = new System.Drawing.Size(75, 23);
+            this.resetBtn.TabIndex = 4;
+            this.resetBtn.Text = "Reset Game\r\n";
+            this.resetBtn.UseVisualStyleBackColor = true;
+            this.resetBtn.Click += new System.EventHandler(this.ResetBtn_Click);
+            // 
+            // door4btn
+            // 
+            this.door4btn.BackgroundImage = global::LetsPlay.Properties.Resources.water_65425408_Cartoon_wooden_door_vector_illustration_free_download_m;
+            this.door4btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.door4btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.door4btn.FlatAppearance.BorderSize = 0;
+            this.door4btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.door4btn.Location = new System.Drawing.Point(382, 123);
+            this.door4btn.Name = "door4btn";
+            this.door4btn.Size = new System.Drawing.Size(92, 184);
+            this.door4btn.TabIndex = 5;
+            this.door4btn.UseVisualStyleBackColor = true;
+            // 
+            // door5btn
+            // 
+            this.door5btn.BackgroundImage = global::LetsPlay.Properties.Resources.water_65425408_Cartoon_wooden_door_vector_illustration_free_download_m;
+            this.door5btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.door5btn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.door5btn.FlatAppearance.BorderSize = 0;
+            this.door5btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.door5btn.Location = new System.Drawing.Point(484, 123);
+            this.door5btn.Name = "door5btn";
+            this.door5btn.Size = new System.Drawing.Size(92, 184);
+            this.door5btn.TabIndex = 6;
+            this.door5btn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 450);
+            this.Controls.Add(this.door5btn);
+            this.Controls.Add(this.door4btn);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.msgLbl);
+            this.Controls.Add(this.door3btn);
+            this.Controls.Add(this.door2btn);
+            this.Controls.Add(this.door1btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button door1btn;
+        private System.Windows.Forms.Button door2btn;
+        private System.Windows.Forms.Button door3btn;
+        private System.Windows.Forms.Label msgLbl;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.Button door4btn;
+        private System.Windows.Forms.Button door5btn;
+    }
+}
+
---
+++ LetsPlay/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LetsPlay
+{
+    public partial class Form1 : Form
+    {
+        string[] prizes = { "goat", "big-screen tv", "car" };
+        Button [] door = new Button[3];
+        int winningDoor;
+        int secondprize;
+        int thirdprize;
+        int tries;
+        public Form1()
+        {
+            InitializeComponent();
+            door[0] = door1btn;
+            door[1] = door2btn;
+            door[2] = door3btn;
+            tries = 2; 
+            winningDoor = 0;
+            secondprize = 0;
+            thirdprize = 0;
+
+            randomizePrize();
+            Thread.Sleep(400);
+            randomizePrize();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Door1btn_Click(object sender, EventArgs e)
+        {
+            bargainPrompt(1);
+        }
+
+        private void Door2btn_Click(object sender, EventArgs e)
+        {
+            bargainPrompt(2);
+        }
+
+        private void Door3btn_Click(object sender, EventArgs e)
+        {
+            bargainPrompt(3);
+        }
+        private void ResetBtn_Click(object sender, EventArgs e)
+        {
+
+            randomizePrize();
+            Thread.Sleep(400);
+            randomizePrize();
+
+        }
+        public void bargainPrompt(int index)
+        {
+            tries--;
+            if (tries != 0)
+            {
+                string msg = "";
+                if (index == winningDoor)
+                {
+                    int randomHint = new Random().Next(0,1);
+                    
+                    if(randomHint==0)
+                        msg = String.Format("Door {0} has a {1} in it, are you sure with your answer?", thirdprize, prizes[0]);
+                    else if(randomHint==1)
+                        msg = String.Format("Door {0} has a {1} in it, are you sure with your answer?", secondprize, prizes[1]);
+                }
+                else if (index == secondprize)
+                {
+                    msg = String.Format("Door {0} has a {1} in it, are you sure with your answer?", thirdprize, prizes[0]);
+                }
+                else if (index == thirdprize)
+                {
+                    msg = String.Format("Door {0} has a {1} in it, are you sure with your answer?", secondprize, prizes[1]);
+                }
+                msgLbl.Text = msg;
+            }
+            else
+            {
+                showPrice(index);
+            }
+        }
+        public void showPrice(int index)
+        {
+            msgLbl.Text = String.Format("Congratulations, you won a {0}", door[index-1].Tag);
+        }
+        public void randomizePrize()
+        {
+            
+            winningDoor = new Random().Next(1, 3);
+            door[winningDoor - 1].Tag = prizes[2];
+            
+            for (; ; ){
+                secondprize = new Random().Next(1, 3);
+                if(secondprize != winningDoor)
+                {
+                    break;
+                }
+            }
+            door[secondprize - 1].Tag = prizes[1];
+            
+            if (winningDoor - secondprize < 0)
+            {
+                thirdprize = secondprize + winningDoor;
+            }
+            else if (winningDoor - secondprize > 0)
+            {
+                thirdprize = winningDoor + secondprize;
+            }
+            door[thirdprize - 1].Tag = prizes[0];
+
+            tries = 2;
+
+
+        }
+
+        private void MsgLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
